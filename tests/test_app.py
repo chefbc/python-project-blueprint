e259bc6e81a3ce6f2a7ffb7af6805fc2cb5095fe
Casey Carlsen <casey.carlsen@kohls.com>
2020-06-11 10:51:25 -0500

    config tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,7 +3,7 @@ from .context import mytest
 
 def test_app(capsys, example_fixture):
     # pylint: disable=W0612,W0613
-    blueprint.Blueprint.run()
+    mytest.Blueprint.run()
     captured = capsys.readouterr()
 
     assert "Hello World..." in captured.out
